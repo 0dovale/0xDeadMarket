42c15d1a3286f64be023d4f7d4477146e59b3ff2
DoVale.exe <84135635+0dovale@users.noreply.github.com>
2025-04-22 15:17:37 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Animação de entrada nas páginas
-document.addEventListener("DOMContentLoaded", () => {
-  const main = document.querySelector("main");
-  if (main) {
-    main.classList.add("fade-in");
-  }
-});
